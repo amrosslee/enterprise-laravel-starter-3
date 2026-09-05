d27d642b37ed2e53009ed0276657a55df9d9c22f
Amr Saleh <dev.amr.saleh@gmail.com>
2026-09-05 16:38:44 +0300

    Filament: Task Resource

---
+++ app/Filament/Resources/Tasks/Schemas/TaskForm.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\Tasks\Schemas;
+
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class TaskForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Select::make('project_id')
+                    ->relationship('project', 'title')
+                    ->required(),
+                TextInput::make('title')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+                Toggle::make('is_complete')
+                    ->required(),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Tasks/Tables/TasksTable.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Filament\Resources\Tasks\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ForceDeleteBulkAction;
+use Filament\Actions\RestoreBulkAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Table;
+
+class TasksTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('project.title')
+                    ->label('Project')
+                    ->sortable(),
+                TextColumn::make('title')
+                    ->searchable(),
+                IconColumn::make('is_complete')
+                    ->boolean(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                TrashedFilter::make(),
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+}
