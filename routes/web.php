0892e44eb7d2f11920aef75e92e79e3ebd281bd1
Amr Saleh <dev.amr.saleh@gmail.com>
2026-09-05 16:34:54 +0300

    replace fortify login with filament login page

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'welcome')->name('home');
+// Route::inertia('/', 'welcome')->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'dashboard')->name('dashboard');
-});
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+// });
 
-require __DIR__.'/settings.php';
+// require __DIR__.'/settings.php';
+
+Route::get('/', fn () => redirect()->route('filament.admin.auth.login'))->name('home');
