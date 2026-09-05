ea30852b6cd1f16076c874481e171a492c5f4443
Amr Saleh <dev.amr.saleh@gmail.com>
2026-09-05 16:37:54 +0300

    Filament: User Resource

---
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources\Users\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ForceDeleteBulkAction;
+use Filament\Actions\RestoreBulkAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Table;
+
+class UsersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('email')
+                    ->label('Email address')
+                    ->searchable(),
+                // TextColumn::make('email_verified_at')
+                //     ->dateTime()
+                //     ->sortable(),
+                // TextColumn::make('two_factor_confirmed_at')
+                //     ->dateTime()
+                //     ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                // TextColumn::make('company_id')
+                //     ->numeric()
+                //     ->sortable(),
+                IconColumn::make('is_active')
+                    ->boolean(),
+                TextColumn::make('company.name')
+                    ->label('Company')
+                    ->sortable(),
+                // TextColumn::make('myrole')
+                //     ->label('Role')
+                //     ->sortable()
+                //     ->toggleable(isToggledHiddenByDefault: false),
+            ])
+            ->filters([
+                TrashedFilter::make(),
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+}
